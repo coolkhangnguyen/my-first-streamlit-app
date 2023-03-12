d8deff3ca0301907bf98c319cba7863d29909f37
coolkhangnguyen <125427167+coolkhangnguyen@users.noreply.github.com>
2023-03-12 16:30:29 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,9 +3,9 @@ import pickle
 import numpy as np
 model=pickle.load(open('model.pickle','rb'))
 st.title("Revenue Prediction")
-x=np.array(st.number_input('Input Temperature'))
+x=st.number_input('Input Temperature')
 if st.button('Predict'):
-  x=x.reshape(-1,1)
+  x=np.array(x).reshape(-1,1)
   result=model.predict(x)
   st.caption('Revenue Prediction')
   ket_qua=result[0]
