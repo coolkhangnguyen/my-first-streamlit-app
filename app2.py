70b81298580d4b29c4c0e9ee8584b64a033e4a29
coolkhangnguyen <125427167+coolkhangnguyen@users.noreply.github.com>
2023-03-12 15:54:37 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,7 +4,8 @@ model=pickle.load(open('model.pickle','rb'))
 st.title("Revenue Prediction")
 x=st.number_input('Input Temperature')
 if st.button('Predict'):
-  result=model.predict(x)
+  x=np.array(x)
+  result=model.predict(x.reshape(-1,1)
   st.caption('Revenue Prediction')
   st.success(result)
 
